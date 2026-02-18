b363ceea27c72f9126574418e96a091cae559c21
Kondrich <123kondrich@gmail.com>
2026-02-18 16:25:59 +0200

    asd

--- variables/not_and_or.py
+++ variables/not_and_or.py
@@ -1,12 +1,40 @@
 # Операторы NOT , AND , OR
+# Приоритетность Выполнения NOT -> AND -> OR
 
+
+# ---   NOT   ---------------------------------------------------------------------------------------
 # оператор not переворачивает значение
+
 # not True  -> False
 # not False -> True
-not 4 == 4  # ->False
+
+not 4 == 4  # -> False
 not 4 != 4  # -> True
 
+# ---   AND   ---------------------------------------------------------------------------------------
 # оператор and возвращает True только если оба значения True
 # в других случаях возвращает False
-# True and True -> True
+
+# True  and True  -> True
+# False and False -> False
+# True  and False -> False
+# False and True  -> False
+
 1 > 0 and 2 > 0  # -> True
+1 < 0 and 2 < 0  # -> False
+1 > 0 and 2 < 0  # -> False
+1 < 0 and 2 > 0  # -> False
+
+
+# ---   OR   ----------------------------------------------------------------------------------------
+# or возвращает True, если хотябы одно условие возвращает True
+
+# True  or True  -> True
+# False or False -> False
+# True  or False -> True
+# False or True  -> True
+
+1 > 0 or 2 > 0  # -> True
+1 < 0 or 2 < 0  # -> False
+1 > 0 or 2 < 0  # -> True
+1 < 0 or 2 > 0  # -> True
