88c4533818e5a4ce54c38b07a262e6d8b4c348fa
Kondrich <123kondrich@gmail.com>
2026-02-21 17:00:47 +0200

    asdasd

--- TEST.py
+++ TEST.py
@@ -1,2 +1,1 @@
-x = 5 > 2
-print(x)
+print("abc"+"fff")
---
+++ variables/type_variables/concatenate_string.py
@@ -0,0 +1,11 @@
+# Конкатенация - обьединение строк
+
+a = "hello"
+b = "world"
+
+a + b  # -> helloworld
+a + " - " + b  # -> hello - world
+
+"c" * 3  # -> ccc
+
+print(str(7))
